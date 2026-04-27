2d3e0617c0c94229b85bbbd4de2e9f4e87544564
stivenescobarg <stivenescobar240208@gmail.com>
2026-04-27 12:45:35 -0500

    Archivo Main.kt, ejecutable

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import ejercicios.*
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main() {
+    println("=== INICIANDO TALLER DE KOTLIN ===")
+    println()
+
+    ejecutarBloque1()
+    println()
+    ejecutarBloque2()
+    println()
+    ejecutarBloque3()
+    println()
+    ejecutarBloque4()
+    println()
+    ejecutarBloque5()
+    println()
+    ejecutarBloque6()
+
+    println()
+    println("=== FIN DEL TALLER ===")
 }
